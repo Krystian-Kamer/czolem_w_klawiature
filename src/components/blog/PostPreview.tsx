eaff80a74f8b1900cdb180ddfa2f02da8029b989
Krystian <texen24@gmail.com>
2025-01-06 18:09:02 +0100

    add changes also to PostPreview Component

--- src/components/blog/PostPreview.tsx
+++ src/components/blog/PostPreview.tsx
@@ -22,7 +22,7 @@ const PostPreview = ({ post }: { post: PostType }) => {
         src={imageSmall}
         loading="lazy"
         alt="post image"
-        className="rounded-t-lg border-x-[1px] border-t-[1px] border-primary sm:border-none border-transparent object-cover opacity-95 sm:prose sm:w-5/6 sm:self-center md:hidden md:w-2/6 md:rounded-l-badge md:rounded-tr-none"
+        className="rounded-t-lg border-x-[1px] border-t-[1px] border-primary sm:border-none sm:border-transparent object-cover opacity-95 sm:prose sm:w-5/6 sm:self-center md:hidden md:w-2/6 md:rounded-l-badge md:rounded-tr-none"
       />
       <img
         src={imageBig}
@@ -31,7 +31,7 @@ const PostPreview = ({ post }: { post: PostType }) => {
         onClick={() => navigate(`/blog/${id}`)}
         className="cursor-custom-pointer hidden rounded-t-lg object-cover opacity-95 duration-300 hover:opacity-85 md:flex md:w-2/5 md:rounded-l-badge md:rounded-tr-none lg:w-2/6"
       />
-      <div className="prose flex h-full flex-col self-center rounded-b-lg border-[1px] border-primary sm:border-none border-transparent bg-white/80 px-4 py-3 sm:w-5/6 md:w-3/5 md:rounded-r-badge md:rounded-bl-none md:p-8 lg:w-3/6">
+      <div className="prose flex h-full flex-col self-center rounded-b-lg border-[1px] border-primary sm:border-none sm:border-transparent bg-white/80 px-4 py-3 sm:w-5/6 md:w-3/5 md:rounded-r-badge md:rounded-bl-none md:p-8 lg:w-3/6">
         <div className="prose">
           <h3
             className={`font-protest ${windowHeight <= 320 && "text-lg"} text-2xl uppercase tracking-wide selection:bg-accent md:text-2xl lg:text-3xl`}
